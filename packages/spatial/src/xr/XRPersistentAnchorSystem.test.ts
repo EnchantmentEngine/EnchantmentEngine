cfbad33412a95902c2c813dd7f10b5389017b37d
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-11-15 10:57:32 +0100

    tst: Unit Tests for `XRPersistentAnchorSystem.ts` (phase 2)

--- packages/spatial/src/xr/XRPersistentAnchorSystem.test.ts
+++ packages/spatial/src/xr/XRPersistentAnchorSystem.test.ts
@@ -23,19 +23,97 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import { afterEach, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, assert, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest'
 import { destroyEmulatedXREngine, mockEmulatedXREngine } from '../../tests/util/mockEmulatedXREngine'
 import { CustomWebXRPolyfill } from '../../tests/webxr/emulator'
 
 import { SystemDefinitions, SystemUUID, createEngine, destroyEngine } from '@ir-engine/ecs'
-import { XRPersistentAnchorSystem } from './XRPersistentAnchorSystem'
+import { getMutableState, getState } from '@ir-engine/hyperflux'
+import { Quaternion, Vector3 } from 'three'
+import { MockXRAnchor } from '../../tests/util/MockXR'
+import { XRAnchorFunctions, XRPersistentAnchorSystem } from './XRPersistentAnchorSystem'
+import { ReferenceSpace, XRState } from './XRState'
 import { XRSystem } from './XRSystem'
 
 /** @note Runs once on the `describe` implied by vitest for this file */
-// @todo
 beforeAll(() => {
   new CustomWebXRPolyfill()
 })
+
+describe('XRAnchorFunctions', () => {
+  describe('createAnchor', () => {
+    beforeEach(async () => {
+      createEngine()
+      await mockEmulatedXREngine()
+    })
+
+    afterEach(() => {
+      destroyEmulatedXREngine()
+      destroyEngine()
+    })
+
+    it('should call `@param xrFrame`.createAnchor and return its result if both `@param xrFrame` and ReferenceSpace.origin are truthy', async () => {
+      const Expected = new MockXRAnchor()
+      // Set the data as expected
+      Expected.delete = vi.fn()
+      const xrFrame = getState(XRState).xrFrame
+      const createAnchorSpy = vi.fn(
+        (_pose: XRRigidTransform, _space: XRSpace) => new Promise<XRAnchor>((resolve) => resolve(Expected as XRAnchor))
+      )
+      getMutableState(XRState).xrFrame.merge({ createAnchor: createAnchorSpy })
+      // Sanity check before running
+      assert(xrFrame)
+      expect(xrFrame).toBeTruthy()
+      expect(ReferenceSpace.origin).toBeTruthy()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(0)
+      expect(Expected.delete).toHaveBeenCalledTimes(0)
+      createAnchorSpy({} as XRRigidTransform, {} as XRSpace)
+      Expected.delete()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(1)
+      expect(Expected.delete).toHaveBeenCalledTimes(1)
+      // Run and Check the result
+      const result = await XRAnchorFunctions.createAnchor(xrFrame, new Vector3(), new Quaternion())
+      expect(result).toBeTruthy()
+      expect(result?.delete).toBe(Expected.delete)
+      result?.delete()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(2)
+      expect(Expected.delete).toHaveBeenCalledTimes(2)
+    })
+
+    it('should throw an error if either of `@param xrFrame` and ReferenceSpace.origin is falsy', async () => {
+      const Expected = new MockXRAnchor()
+      // Set the data as expected
+      Expected.delete = vi.fn()
+      const xrFrame = getState(XRState).xrFrame
+      const createAnchorSpy = vi.fn(
+        (_pose: XRRigidTransform, _space: XRSpace) => new Promise<XRAnchor>((resolve) => resolve(Expected as XRAnchor))
+      )
+      getMutableState(XRState).xrFrame.merge({ createAnchor: createAnchorSpy })
+      ReferenceSpace.origin = null
+      // Sanity check before running
+      assert(xrFrame)
+      expect(xrFrame).toBeTruthy()
+      expect(ReferenceSpace.origin).not.toBeTruthy()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(0)
+      expect(Expected.delete).toHaveBeenCalledTimes(0)
+      // Run and Check the result
+      try {
+        await XRAnchorFunctions.createAnchor(xrFrame, new Vector3(), new Quaternion())
+      } catch (error) {
+        expect(error.message).toMatch('XRFrame not available.')
+      }
+      expect(createAnchorSpy).toHaveBeenCalledTimes(0)
+      expect(Expected.delete).toHaveBeenCalledTimes(0)
+    })
+  }) //:: createAnchor
+
+  /** @todo */
+  describe('createPersistentAnchor', () => {}) //:: createPersistentAnchor
+  describe('restoreAnchor', () => {}) //:: restoreAnchor
+  describe('deleteAnchor', () => {}) //:: deleteAnchor
+  describe('anchors', () => {}) //:: anchors
+  describe('anchorPoses', () => {}) //:: anchorPoses
+}) //:: XRAnchorFunctions
 
 describe('XRPersistentAnchorSystem', () => {
   const System = SystemDefinitions.get(XRPersistentAnchorSystem)!
