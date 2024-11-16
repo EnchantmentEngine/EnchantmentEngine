8780c7ae96c0d9674a4d06ce1d939809ba0815f1
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-11-16 06:20:40 +0100

    tst: Unit Tests for `XRPersistentAnchorSystem.ts` (phase 3)

--- packages/spatial/src/xr/XRPersistentAnchorSystem.test.ts
+++ packages/spatial/src/xr/XRPersistentAnchorSystem.test.ts
@@ -30,7 +30,7 @@ import { CustomWebXRPolyfill } from '../../tests/webxr/emulator'
 import { SystemDefinitions, SystemUUID, createEngine, destroyEngine } from '@ir-engine/ecs'
 import { getMutableState, getState } from '@ir-engine/hyperflux'
 import { Quaternion, Vector3 } from 'three'
-import { MockXRAnchor } from '../../tests/util/MockXR'
+import { MockXRAnchor, MockXRFrame } from '../../tests/util/MockXR'
 import { XRAnchorFunctions, XRPersistentAnchorSystem } from './XRPersistentAnchorSystem'
 import { ReferenceSpace, XRState } from './XRState'
 import { XRSystem } from './XRSystem'
@@ -72,7 +72,12 @@ describe('XRAnchorFunctions', () => {
       expect(createAnchorSpy).toHaveBeenCalledTimes(1)
       expect(Expected.delete).toHaveBeenCalledTimes(1)
       // Run and Check the result
-      const result = await XRAnchorFunctions.createAnchor(xrFrame, new Vector3(), new Quaternion())
+      let result: XRAnchor | undefined
+      try {
+        result = await XRAnchorFunctions.createAnchor(xrFrame, new Vector3(), new Quaternion())
+      } catch (error) {
+        expect(true).eq(false, "XRAnchorFunctions.createAnchor shouldn't throw an error under the defined conditions.")
+      }
       expect(result).toBeTruthy()
       expect(result?.delete).toBe(Expected.delete)
       result?.delete()
@@ -107,12 +112,300 @@ describe('XRAnchorFunctions', () => {
     })
   }) //:: createAnchor
 
-  /** @todo */
-  describe('createPersistentAnchor', () => {}) //:: createPersistentAnchor
-  describe('restoreAnchor', () => {}) //:: restoreAnchor
-  describe('deleteAnchor', () => {}) //:: deleteAnchor
-  describe('anchors', () => {}) //:: anchors
-  describe('anchorPoses', () => {}) //:: anchorPoses
+  describe('createPersistentAnchor', () => {
+    beforeEach(async () => {
+      createEngine()
+      await mockEmulatedXREngine()
+    })
+
+    afterEach(() => {
+      destroyEmulatedXREngine()
+      destroyEngine()
+    })
+
+    it('should call `@param xrFrame`.createAnchor and return its result inside slot 0 of the returned tuple if both `@param xrFrame` and ReferenceSpace.origin are truthy', async () => {
+      const Expected = new MockXRAnchor()
+      // Set the data as expected
+      Expected.delete = vi.fn()
+      const xrFrame = getState(XRState).xrFrame
+      const createAnchorSpy = vi.fn(
+        (_pose: XRRigidTransform, _space: XRSpace) => new Promise<XRAnchor>((resolve) => resolve(Expected as XRAnchor))
+      )
+      getMutableState(XRState).xrFrame.merge({ createAnchor: createAnchorSpy })
+      // Sanity check before running
+      assert(xrFrame)
+      expect(xrFrame).toBeTruthy()
+      expect(ReferenceSpace.origin).toBeTruthy()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(0)
+      expect(Expected.delete).toHaveBeenCalledTimes(0)
+      createAnchorSpy({} as XRRigidTransform, {} as XRSpace)
+      Expected.delete()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(1)
+      expect(Expected.delete).toHaveBeenCalledTimes(1)
+      // Run the process
+      let resultTuple: [XRAnchor, string | undefined] | undefined = undefined
+      try {
+        resultTuple = await XRAnchorFunctions.createPersistentAnchor(xrFrame, new Vector3(), new Quaternion())
+      } catch (error) {
+        expect(true).eq(
+          false,
+          "XRAnchorFunctions.createPersistentAnchor shouldn't throw an error under the defined conditions."
+        )
+      }
+      // Check the result
+      assert(resultTuple)
+      expect(typeof resultTuple[0]).not.toBe('string') // The XRAnchor is at result[0]
+      const result = resultTuple[0] as XRAnchor
+      expect(result).toBeTruthy()
+      expect(result?.delete).toBe(Expected.delete)
+      result?.delete()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(2)
+      expect(Expected.delete).toHaveBeenCalledTimes(2)
+    })
+
+    it('should call anchor.requestPersistentHandle and return its result inside slot 1 of the returned tuple if both `@param xrFrame` and ReferenceSpace.origin are truthy', async () => {
+      const Expected = 'arstarst'
+      const requestPersistentHandle_ShouldError = false
+      // Set the data as expected
+      const Anchor = new MockXRAnchor()
+      Anchor.delete = vi.fn()
+      Anchor['requestPersistentHandle'] = vi.fn(() => {
+        return new Promise<string>((resolve) => {
+          if (requestPersistentHandle_ShouldError) throw new Error(Expected)
+          else resolve(Expected)
+        })
+      })
+      const xrFrame = getState(XRState).xrFrame
+      const createAnchorSpy = vi.fn(
+        (_pose: XRRigidTransform, _space: XRSpace) => new Promise<XRAnchor>((resolve) => resolve(Anchor as XRAnchor))
+      )
+      getMutableState(XRState).xrFrame.merge({ createAnchor: createAnchorSpy })
+      // Sanity check before running
+      assert(xrFrame)
+      expect(xrFrame).toBeTruthy()
+      expect(ReferenceSpace.origin).toBeTruthy()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(0)
+      expect(Anchor.delete).toHaveBeenCalledTimes(0)
+      createAnchorSpy({} as XRRigidTransform, {} as XRSpace)
+      Anchor.delete()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(1)
+      expect(Anchor.delete).toHaveBeenCalledTimes(1)
+      // Run the process
+      let resultTuple: [XRAnchor, string | undefined] | undefined = undefined
+      try {
+        resultTuple = await XRAnchorFunctions.createPersistentAnchor(xrFrame, new Vector3(), new Quaternion())
+      } catch (error) {
+        expect(true).eq(
+          false,
+          "XRAnchorFunctions.createPersistentAnchor shouldn't throw an error under the defined conditions."
+        )
+      }
+      // Check the result
+      assert(resultTuple)
+      expect(typeof resultTuple[1]).toBe('string') // The XRAnchor is at result[0]
+      expect(createAnchorSpy).toHaveBeenCalledTimes(2)
+      expect(Anchor.delete).toHaveBeenCalledTimes(1)
+      const result = resultTuple[1]
+      expect(result).toBe(Expected)
+    })
+
+    it('should call anchor.delete and throw the error thrown by anchor.requestPersistentHandle when anchor.requestPersistentHandle failed and both `@param xrFrame` and ReferenceSpace.origin are truthy', async () => {
+      const Expected = 'arstarst'
+      const requestPersistentHandle_ShouldError = true
+      // Set the data as expected
+      const Anchor = new MockXRAnchor()
+      Anchor.delete = vi.fn()
+      Anchor['requestPersistentHandle'] = vi.fn(() => {
+        return new Promise<string>((resolve) => {
+          if (requestPersistentHandle_ShouldError) throw new Error(Expected)
+          else resolve(Expected)
+        })
+      })
+      const xrFrame = getState(XRState).xrFrame
+      const createAnchorSpy = vi.fn(
+        (_pose: XRRigidTransform, _space: XRSpace) => new Promise<XRAnchor>((resolve) => resolve(Anchor as XRAnchor))
+      )
+      getMutableState(XRState).xrFrame.merge({ createAnchor: createAnchorSpy })
+      // Sanity check before running
+      assert(xrFrame)
+      expect(xrFrame).toBeTruthy()
+      expect(ReferenceSpace.origin).toBeTruthy()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(0)
+      expect(Anchor.delete).toHaveBeenCalledTimes(0)
+      createAnchorSpy({} as XRRigidTransform, {} as XRSpace)
+      Anchor.delete()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(1)
+      expect(Anchor.delete).toHaveBeenCalledTimes(1)
+      // Run the process
+      let resultTuple: [XRAnchor, string | undefined] | undefined = undefined
+      try {
+        resultTuple = await XRAnchorFunctions.createPersistentAnchor(xrFrame, new Vector3(), new Quaternion())
+        expect(true).eq(
+          false,
+          'XRAnchorFunctions.createPersistentAnchor should throw an error under the defined conditions.'
+        )
+      } catch (error) {
+        // Check the result
+        expect(resultTuple).toBe(undefined)
+        expect(createAnchorSpy).toHaveBeenCalledTimes(2)
+        expect(Anchor.delete).toHaveBeenCalledTimes(2)
+      }
+    })
+
+    it('should throw an error if either of `@param xrFrame` and ReferenceSpace.origin is falsy', async () => {
+      const Expected = new MockXRAnchor()
+      // Set the data as expected
+      Expected.delete = vi.fn()
+      const xrFrame = getState(XRState).xrFrame
+      const createAnchorSpy = vi.fn(
+        (_pose: XRRigidTransform, _space: XRSpace) => new Promise<XRAnchor>((resolve) => resolve(Expected as XRAnchor))
+      )
+      getMutableState(XRState).xrFrame.merge({ createAnchor: createAnchorSpy })
+      ReferenceSpace.origin = null
+      // Sanity check before running
+      assert(xrFrame)
+      expect(xrFrame).toBeTruthy()
+      expect(ReferenceSpace.origin).not.toBeTruthy()
+      expect(createAnchorSpy).toHaveBeenCalledTimes(0)
+      expect(Expected.delete).toHaveBeenCalledTimes(0)
+      // Run the process
+      try {
+        await XRAnchorFunctions.createPersistentAnchor(xrFrame, new Vector3(), new Quaternion())
+      } catch (error) {
+        expect(error.message).toMatch('XRFrame not available.')
+      }
+      // Check the result
+      expect(createAnchorSpy).toHaveBeenCalledTimes(0)
+      expect(Expected.delete).toHaveBeenCalledTimes(0)
+    })
+  }) //:: createPersistentAnchor
+
+  describe('restoreAnchor', () => {
+    beforeEach(async () => {
+      createEngine()
+      await mockEmulatedXREngine()
+    })
+
+    afterEach(() => {
+      destroyEmulatedXREngine()
+      destroyEngine()
+    })
+
+    /** @todo Why is the session undefined, when it is being initialized in mockEmulatedXREngine ?? */
+    it.skip('should call xrFrame.session.restorePersistentAnchor with `@param uuid` when `@param xrFrame.session` is truthy', async () => {
+      // Set the data as expected
+      const xrFrame = getState(XRState).xrFrame
+      const uuid = 'starstars'
+      // Sanity check before running
+      assert(xrFrame)
+      expect(xrFrame.session).toBeTruthy()
+      expect(xrFrame.session).not.toBe(null)
+      expect(xrFrame.session).not.toBe(undefined)
+      // Run the process
+      try {
+        await XRAnchorFunctions.restoreAnchor(xrFrame, uuid)
+      } catch (error) {
+        expect(true).eq(
+          false,
+          'XRAnchorFunctions.restoreAnchor should not throw an error under the defined conditions.'
+        )
+      }
+      // Check the result
+    })
+
+    it('should throw an error if `@param xrFrame.session` is falsy', async () => {
+      // Set the data as expected
+      const mockedXRFrame = new MockXRFrame()
+      mockedXRFrame['session'] = null
+      // @ts-expect-error Allow coercing MockXRFrame into XRFrame
+      const xrFrame = mockedXRFrame as XRFrame
+      const uuid = 'starstars'
+      // Sanity check before running
+      expect(xrFrame.session).toBeFalsy()
+      expect(xrFrame.session).toBe(null)
+      // Run the process
+      try {
+        await XRAnchorFunctions.restoreAnchor(xrFrame, uuid)
+        expect(true).eq(false, 'XRAnchorFunctions.restoreAnchor should throw an error under the defined conditions.')
+      } catch (error) {
+        // Check the result
+        expect(error.message).toMatch('XRSession not available.')
+      }
+    })
+  }) //:: restoreAnchor
+
+  describe('deleteAnchor', () => {
+    beforeEach(async () => {
+      createEngine()
+      await mockEmulatedXREngine()
+    })
+
+    afterEach(() => {
+      destroyEmulatedXREngine()
+      destroyEngine()
+    })
+
+    /** @todo Why is the session undefined, when it is being initialized in mockEmulatedXREngine ?? */
+    it.skip('should call xrFrame.session.deletePersistentAnchor with `@param uuid` when `@param xrFrame.session` is truthy', async () => {
+      // Set the data as expected
+      const xrFrame = getState(XRState).xrFrame
+      const uuid = 'starstars'
+      // Sanity check before running
+      assert(xrFrame)
+      expect(xrFrame.session).toBeTruthy()
+      expect(xrFrame.session).not.toBe(null)
+      expect(xrFrame.session).not.toBe(undefined)
+      // Run the process
+      try {
+        await XRAnchorFunctions.restoreAnchor(xrFrame, uuid)
+      } catch (error) {
+        expect(true).eq(false, 'XRAnchorFunctions.deleteAnchor should not throw an error under the defined conditions.')
+      }
+      // Check the result
+    })
+
+    it('should throw an error if `@param xrFrame.session` is falsy', async () => {
+      // Set the data as expected
+      const mockedXRFrame = new MockXRFrame()
+      mockedXRFrame['session'] = null
+      // @ts-expect-error Allow coercing MockXRFrame into XRFrame
+      const xrFrame = mockedXRFrame as XRFrame
+      const uuid = 'starstars'
+      // Sanity check before running
+      expect(xrFrame.session).toBeFalsy()
+      expect(xrFrame.session).toBe(null)
+      // Run the process
+      try {
+        await XRAnchorFunctions.deleteAnchor(xrFrame, uuid)
+        expect(true).eq(false, 'XRAnchorFunctions.deleteAnchor should throw an error under the defined conditions.')
+      } catch (error) {
+        // Check the result
+        expect(error.message).toMatch('XRSession not available.')
+      }
+    })
+  }) //:: deleteAnchor
+
+  describe('anchors', () => {
+    it('should start empty', () => {
+      expect(XRAnchorFunctions.anchors.size).toBe(0)
+    })
+
+    it('should have the correct type', () => {
+      expect(typeof XRAnchorFunctions.anchors).toBe('object')
+      expect(XRAnchorFunctions.anchors).toBeInstanceOf(Set)
+    })
+  }) //:: anchors
+
+  describe('anchorPoses', () => {
+    it('should start empty', () => {
+      expect(XRAnchorFunctions.anchorPoses.size).toBe(0)
+    })
+
+    it('should have the correct type', () => {
+      expect(typeof XRAnchorFunctions.anchorPoses).toBe('object')
+      expect(XRAnchorFunctions.anchorPoses).toBeInstanceOf(Map)
+    })
+  }) //:: anchorPoses
 }) //:: XRAnchorFunctions
 
 describe('XRPersistentAnchorSystem', () => {
@@ -144,5 +437,6 @@ describe('XRPersistentAnchorSystem', () => {
     })
   }) //:: Fields
 
+  /** @todo Very Branchy */
   describe('execute', () => {}) //:: execute
 }) //:: XRPersistentAnchorSystem
