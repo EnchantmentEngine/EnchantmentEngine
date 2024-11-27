72a4894244af68f2a5857f8d8ec2d6b25c1cf136
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-11-27 17:28:46 +0100

    tst: List of missing todo cases for `XRDepthOcclussion.ts`

--- packages/spatial/src/xr/XRDepthOcclusion.test.tsx
+++ packages/spatial/src/xr/XRDepthOcclusion.test.tsx
@@ -28,7 +28,7 @@ import { destroyEmulatedXREngine, mockEmulatedXREngine } from '../../tests/util/
 import { CustomWebXRPolyfill } from '../../tests/webxr/emulator'
 
 import { SystemDefinitions, SystemUUID, createEngine, destroyEngine } from '@ir-engine/ecs'
-import { XRDepthOcclusionSystem } from './XRDepthOcclusion'
+import { XRDepthOcclusion, XRDepthOcclusionSystem } from './XRDepthOcclusion'
 import { XRSystem } from './XRSystem'
 
 /** @note Runs once on the `describe` implied by vitest for this file */
@@ -37,12 +37,74 @@ beforeAll(() => {
 })
 
 describe('XRDepthOcclusion', () => {
-  /** @todo */
-  describe('XRDepthOcclusionMaterials,', () => {}) //:: XRDepthOcclusionMaterials
-  describe('addDepthOBCPlugin,', () => {}) //:: addDepthOBCPlugin
-  describe('removeDepthOBCPlugin,', () => {}) //:: removeDepthOBCPlugin
-  describe('updateDepthMaterials,', () => {}) //:: updateDepthMaterials
-  describe('updateUniforms', () => {}) //:: updateUniforms
+  describe('XRDepthOcclusionMaterials,', () => {
+    it('should start as an empty array of XRDepthOcclusionMaterialType', () => {
+      expect(Array.isArray(XRDepthOcclusion.XRDepthOcclusionMaterials)).toBe(true)
+      expect(XRDepthOcclusion.XRDepthOcclusionMaterials.length).toBe(0)
+    })
+  }) //:: XRDepthOcclusionMaterials
+
+  describe('addDepthOBCPlugin,', () => {
+    /**
+    // @todo
+    it("should not do anything if `@param material` is falsy", () => {})
+    it("should set `@param material`.userData to an empty object if its falsy", () => {})
+    it("should exit early if `@param material`.userData.DepthOcclusionPlugin", () => {})
+    describe("`@param material`.userData.DepthOcclusionPlugin", () => {
+      it("should set .id to the expected default value", () => {})
+      it("should set .uniforms to the expected default value", () => {})
+      describe("compile", () => {
+        it("should not do anything if mat.userData.DepthOcclusionPlugin is falsy", () => {})
+        describe("for all uniforms of DepthOcclusionPlugin", () => {
+          it(".. should add all `@param material`.userData.DepthOcclusionPlugin.uniforms to `@param shader`.uniforms", () => {})
+        })
+        it("should set `@param material`.shader to `@param shader`", () => {})
+        it("should set `@param shader`.fragmentShader to the expected value", () => {})
+        it("should set `@param shader`.vertexShader to the expected value", () => {})
+      }) //:: DepthOcclusionPlugin.compile
+    }) //:: DepthOcclusionPlugin
+    it("should call addOBCPlugin with `@param material` and `@param material`.userData.DepthOcclusionPlugin", () => {})
+    it("should set `@param material`.needsUpdate to true", () => {})
+    it("should add the `@param material` to the XRDepthOcclusionMaterials list", () => {})
+    */
+  }) //:: addDepthOBCPlugin
+
+  describe('removeDepthOBCPlugin,', () => {
+    /**
+    // @todo
+    it("should not do anything if `@param material`.userData.DepthOcclusionPlugin is falsy", () => {})
+    it("should call removeOBCPlugin with `@param material` and `@param material`.userData.DepthOcclusionPlugin", () => {})
+    it("should remove the DepthOcclusionPlugin field from `@param material`.userData", () => {})
+    it("should remove the `@param material` from the XRDepthOcclusionMaterials list", () => {})
+    it("should set `@param material`.needsUpdate to true", () => {})
+    */
+  }) //:: removeDepthOBCPlugin
+
+  describe('updateDepthMaterials,', () => {
+    /**
+    // @todo
+    it("should not do anything if either `@param frame` or `@param referenceSpace` are falsy", () => {})
+    it("should not do anything if `@param frame`.getViewerPose(`@param referenceSpace`) is falsy", () => {})
+    describe("for every viewerPose in `@param frame`.getViewerPose(`@param referenceSpace`)", () => {
+      it(".. should not do anything for the view if frame.getDepthInformation(view) is falsy", () => {})
+      it(".. should set XRState.depthDataTexture to a new DepthDataTexture created with (depthInfo.width, depthInfo.height) if its value is falsy", () => {})
+      it(".. should call XRState.depthDataTexture.updateDepth with the value of frame.getDepthInformation(view)", () => {})
+      it(".. should call XRDepthOcclusion.updateUniforms with XRDepthOcclusionMaterials and the value of frame.getDepthInformation(view)", () => {})
+      it(".. should call `@param depthTexture`.updateDepth with the value of frame.getDepthInformation(view)", () => {})
+    })
+    */
+  }) //:: updateDepthMaterials
+
+  describe('updateUniforms', () => {
+    /**
+    // @todo
+    it("should not do anything for a material if mat.userData.DepthOcclusionPlugin is falsy", () => {})
+    it("should not do anything for a material if material.shader is falsy", () => {})
+    it("should set the uResolution uniform of the mat.shader to the expected (width, height) for the window.devicePixelRatio", () => {})
+    it("should set the uUvTransform uniform of the mat.shader to the inverse of `@param depthInfo`.normTextureFromNormViewMatrix.matrix", () => {})
+    it("should set the uRawValueToMeters uniform of the mat.shader to `@param depthInfo`.rawValueToMeters", () => {})
+    */
+  }) //:: updateUniforms
 }) //:: XRDepthOcclusion
 
 describe('XRDepthOcclusionSystem', () => {
