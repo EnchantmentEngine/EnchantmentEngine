db8165cfcc86d19c44d055d567971bc8fad123ee
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-12-20 12:29:03 +0100

    fmt: Call `updateSession*` functions from `XRSystemFunctions`
    Reason: Expose them from an object, to be reachable from unit tests with `vi.spyOn`

--- packages/spatial/src/xr/XRSystem.ts
+++ packages/spatial/src/xr/XRSystem.ts
@@ -43,9 +43,9 @@ const updateSessionSupportForMode = (mode: XRSessionMode) => {
 }
 
 const updateSessionSupport = () => {
-  updateSessionSupportForMode('inline')
-  updateSessionSupportForMode('immersive-ar')
-  updateSessionSupportForMode('immersive-vr')
+  XRSystemFunctions.updateSessionSupportForMode('inline')
+  XRSystemFunctions.updateSessionSupportForMode('immersive-ar')
+  XRSystemFunctions.updateSessionSupportForMode('immersive-vr')
 }
 
 const xrSessionChangedQueue = defineActionQueue(XRAction.sessionChanged.matches)
@@ -56,11 +56,11 @@ const execute = () => {
 
 const reactor = () => {
   useEffect(() => {
-    navigator.xr?.addEventListener('devicechange', updateSessionSupport)
-    updateSessionSupport()
+    navigator.xr?.addEventListener('devicechange', XRSystemFunctions.updateSessionSupport)
+    XRSystemFunctions.updateSessionSupport()
 
     return () => {
-      navigator.xr?.removeEventListener('devicechange', updateSessionSupport)
+      navigator.xr?.removeEventListener('devicechange', XRSystemFunctions.updateSessionSupport)
     }
   }, [])
   return null
@@ -72,3 +72,9 @@ export const XRSystem = defineSystem({
   execute,
   reactor
 })
+
+/** @note Internal usage. Exported for unit tests. */
+export const XRSystemFunctions = {
+  updateSessionSupportForMode,
+  updateSessionSupport
+}
