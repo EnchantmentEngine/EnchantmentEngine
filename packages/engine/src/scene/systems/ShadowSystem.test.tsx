ace87fb902019794613fa39af310f16159a9f6ac
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2025-02-11 07:25:58 +0100

    tst: Unit Tests for `ShadowSystem.tsx` (phase 1)

--- packages/engine/src/scene/systems/ShadowSystem.test.tsx
+++ packages/engine/src/scene/systems/ShadowSystem.test.tsx
@@ -23,14 +23,44 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import { afterEach, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest'
+import { assertVec } from '../../../../spatial/tests/util/assert'
 import { destroyEmulatedXREngine, mockEmulatedXREngine } from '../../../../spatial/tests/util/mockEmulatedXREngine'
 import { CustomWebXRPolyfill } from '../../../../spatial/tests/webxr/emulator'
 
-import { AnimationSystemGroup, createEngine, destroyEngine, SystemDefinitions, SystemUUID } from '@ir-engine/ecs'
-import { TransformSystem } from '@ir-engine/spatial'
+import {
+  AnimationSystemGroup,
+  createEngine,
+  createEntity,
+  destroyEngine,
+  getComponent,
+  getMutableComponent,
+  removeEntity,
+  setComponent,
+  SystemDefinitions,
+  SystemUUID,
+  UndefinedEntity
+} from '@ir-engine/ecs'
+import { getMutableState } from '@ir-engine/hyperflux'
+import { TransformComponent, TransformSystem } from '@ir-engine/spatial'
+import { Vector3_Back } from '@ir-engine/spatial/src/common/constants/MathConstants'
+import { RendererState } from '@ir-engine/spatial/src/renderer/RendererState'
+import { RendererComponent } from '@ir-engine/spatial/src/renderer/WebGLRendererSystem'
+import { MeshComponent } from '@ir-engine/spatial/src/renderer/components/MeshComponent'
+import { ObjectComponent } from '@ir-engine/spatial/src/renderer/components/ObjectComponent'
+import { VisibleComponent } from '@ir-engine/spatial/src/renderer/components/VisibleComponent'
+import { CSM } from '@ir-engine/spatial/src/renderer/csm/CSM'
+import { getShadowsEnabled } from '@ir-engine/spatial/src/renderer/functions/RenderSettingsFunction'
 import { isMobileXRHeadset } from '@ir-engine/spatial/src/xr/XRState'
-import { DropShadowSystem, ShadowSystem, ShadowSystemState } from './ShadowSystem'
+import { BoxGeometry, Material, Mesh, Quaternion, Raycaster, Vector3 } from 'three'
+import { DropShadowComponent } from '../components/DropShadowComponent'
+import {
+  DropShadowSystem,
+  ShadowSystem,
+  ShadowSystemData,
+  ShadowSystemFunctions,
+  ShadowSystemState
+} from './ShadowSystem'
 
 /** @note Runs once on the `describe` implied by vitest for this file */
 beforeAll(() => {
@@ -108,17 +138,95 @@ describe('ShadowSystem', () => {
     })
   }) //:: Fields
 
-  /** @todo */
   describe('execute', () => {
-    it.todo('should not do anything if the result of getShadowsEnabled is falsy', () => {})
+    let testEntity = UndefinedEntity
+
+    beforeEach(async () => {
+      createEngine()
+      testEntity = createEntity()
+    })
+
+    afterEach(() => {
+      removeEntity(testEntity)
+      destroyEngine()
+    })
+
+    it('should not do anything if the result of getShadowsEnabled is falsy', () => {
+      // Set the data as expected
+      const resultSpy = vi.fn()
+      setComponent(testEntity, RendererComponent)
+      const csm = { update: resultSpy as any } as CSM
+      getMutableComponent(testEntity, RendererComponent).csm.set(csm)
+      getMutableState(RendererState).useShadows.set(false)
+      // Sanity check before running
+      expect(getShadowsEnabled()).toBeFalsy()
+      expect(resultSpy).not.toHaveBeenCalled()
+      // Run and Check the result
+      System.execute()
+      expect(resultSpy).not.toHaveBeenCalled()
+    })
+
     describe('for every entity that has a RendererComponent', () => {
-      it.todo('should call entity.RendererComponent.csm.update if entity.RendererComponent.csm is truthy', () => {})
-      it.todo('should not call entity.RendererComponent.csm.update if entity.RendererComponent.csm is falsy', () => {})
+      it('should call entity.RendererComponent.csm.update if entity.RendererComponent.csm is truthy', () => {
+        // Set the data as expected
+        const resultSpy = vi.fn()
+        setComponent(testEntity, RendererComponent)
+        const csm = { update: resultSpy as any } as CSM
+        getMutableComponent(testEntity, RendererComponent).csm.set(csm)
+        // Sanity check before running
+        expect(getShadowsEnabled()).toBeTruthy()
+        expect(resultSpy).not.toHaveBeenCalled()
+        // Run and Check the result
+        System.execute()
+        expect(resultSpy).toHaveBeenCalled()
+        expect(resultSpy).toHaveBeenCalledTimes(1)
+      })
+
+      it('should not call entity.RendererComponent.csm.update if entity.RendererComponent.csm is falsy', () => {
+        /* @note Just for test coverage %
+         * Can't test that the function wasn't called if csm is falsy,
+         * because assigning the function makes csm truthy.
+         * */
+        // Set the data as expected
+        const resultSpy = vi.fn()
+        setComponent(testEntity, RendererComponent)
+        const csm = null
+        getMutableComponent(testEntity, RendererComponent).csm.set(csm)
+        // Sanity check before running
+        expect(getShadowsEnabled()).toBeTruthy()
+        expect(resultSpy).not.toHaveBeenCalled()
+        // Run and Check the result
+        System.execute()
+        expect(resultSpy).not.toHaveBeenCalled()
+      })
     })
   }) //:: execute
 
   /** @todo */
-  describe('reactor', () => {}) //:: reactor
+  describe('reactor', () => {
+    beforeEach(async () => {
+      createEngine()
+    })
+
+    afterEach(() => {
+      destroyEngine()
+    })
+
+    describe('when shadowTexture changes ..', () => {
+      it.todo('.. should not do anything (return) if shadowTexture is falsy', () => {})
+      it.todo('.. should set _shadowMaterial.map to shadowTexture', () => {})
+      it.todo('.. should set _shadowMaterial.needsUpdate to true', () => {})
+    })
+    it.todo(
+      'should render the RenderSettingsQueryReactor once for every entity that has a RenderSettingsComponent when useShadowsEnabled is truthy',
+      () => {}
+    )
+    it.todo(
+      'should render the DropShadowReactor once for every entity that has the components [VisibleComponent, ShadowComponent] when useShadowsEnabled is falsy and shadowTexture is truthy',
+      () => {}
+    )
+    it.todo('should render the RendererShadowReactor once for every entity that has a RendererComponent', () => {})
+  }) //:: reactor
 }) //:: ShadowSystem
 
 describe('DropShadowSystem', () => {
@@ -140,19 +248,270 @@ describe('DropShadowSystem', () => {
     })
   }) //:: Fields
 
-  /** @todo */
   describe('execute', () => {
-    it.todo(
-      'should call ShadowSystemFunctions.updateDropShadowTransforms when the result of getShadowsEnabled is falsy',
-      () => {}
-    )
-    it.todo(
-      'should not call ShadowSystemFunctions.updateDropShadowTransforms when the result of getShadowsEnabled is truthy',
-      () => {}
-    )
+    beforeEach(async () => {
+      createEngine()
+    })
+
+    afterEach(() => {
+      destroyEngine()
+    })
+
+    it('should call ShadowSystemFunctions.updateDropShadowTransforms when the result of getShadowsEnabled is falsy', () => {
+      // Set the data as expected
+      const resultSpy = vi.spyOn(ShadowSystemFunctions, 'updateDropShadowTransforms')
+      getMutableState(RendererState).useShadows.set(false)
+      // Sanity check before running
+      expect(getShadowsEnabled()).toBeFalsy()
+      expect(resultSpy).not.toHaveBeenCalled()
+      // Run and Check the result
+      System.execute()
+      expect(resultSpy).toHaveBeenCalled()
+      expect(resultSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it('should not call ShadowSystemFunctions.updateDropShadowTransforms when the result of getShadowsEnabled is truthy', () => {
+      // Set the data as expected
+      const resultSpy = vi.spyOn(ShadowSystemFunctions, 'updateDropShadowTransforms')
+      // Sanity check before running
+      expect(getShadowsEnabled()).toBeTruthy()
+      expect(resultSpy).not.toHaveBeenCalled()
+      // Run and Check the result
+      System.execute()
+      expect(resultSpy).not.toHaveBeenCalled()
+    })
   }) //:: execute
 }) //:: DropShadowSystem
 
 describe('ShadowSystemFunctions', () => {
-  describe('updateDropShadowTransforms', () => {}) //:: updateDropShadowTransforms
+  describe('updateDropShadowTransforms', () => {
+    let testEntity = UndefinedEntity
+    beforeEach(() => {
+      createEngine()
+      testEntity = createEntity()
+    })
+
+    afterEach(() => {
+      removeEntity(testEntity)
+      destroyEngine()
+    })
+
+    describe('for every entity in the ShadowSystemState.priorityEntities list ..', () => {
+      let material = undefined as Material | undefined
+      let sceneEntity = UndefinedEntity
+      let priorityEntity = UndefinedEntity
+      let dropShadowEntity = UndefinedEntity
+      const center = new Vector3()
+
+      beforeEach(() => {
+        const sceneEntityPosition = new Vector3(0, 42, 0)
+        sceneEntity = createEntity() // shadowIntersectionEntity : Intersect against
+        setComponent(sceneEntity, MeshComponent, new Mesh(new BoxGeometry()))
+        setComponent(sceneEntity, VisibleComponent)
+        setComponent(sceneEntity, TransformComponent, { position: sceneEntityPosition })
+
+        priorityEntity = createEntity() // shadowCasterEntity
+        setComponent(priorityEntity, VisibleComponent)
+        setComponent(priorityEntity, TransformComponent)
+        setComponent(priorityEntity, DropShadowComponent, { entity: createEntity(), center: center })
+
+        dropShadowEntity = getComponent(priorityEntity, DropShadowComponent).entity
+        setComponent(dropShadowEntity, VisibleComponent)
+        setComponent(dropShadowEntity, TransformComponent)
+        material = new Material()
+        setComponent(dropShadowEntity, ObjectComponent, new Mesh(new BoxGeometry(), material))
+      })
+
+      afterEach(() => {
+        material = undefined
+        center.set(0, 0, 0)
+        removeEntity(sceneEntity)
+        removeEntity(priorityEntity)
+        removeEntity(dropShadowEntity)
+        sceneEntity = UndefinedEntity
+        priorityEntity = UndefinedEntity
+        dropShadowEntity = UndefinedEntity
+      })
+
+      describe('.. when there is no intersection or intersection.face between the scene objects and a ray casted from entity.DropShadowComponent.entity.TransformComponent.position.world ...', () => {
+        it('... should set entity.DropShadowComponent.entity.TransformComponent.scale to (0,0,0)', () => {
+          const Expected = new Vector3(0, 0, 0)
+          const Initial = new Vector3(5, 6, 7)
+
+          // Set the input & dependencies data
+          setComponent(dropShadowEntity, TransformComponent, { scale: Initial })
+          const rayDir = ShadowSystemData.shadowDirection.clone()
+          const rayPos = TransformComponent.getWorldPosition(priorityEntity, new Vector3()).add(center)
+          const raycaster = new Raycaster(rayPos, rayDir)
+          const sceneObjects = [getComponent(sceneEntity, MeshComponent)]
+          const intersected = raycaster.intersectObjects(sceneObjects, false)[0]
+
+          // Sanity check (input & dependencies)
+          expect(intersected).toBeFalsy()
+          const before = getComponent(dropShadowEntity, TransformComponent).scale
+          assertVec.approxEq(before, Initial, 4)
+          assertVec.anyApproxNotEq(before, Expected, 4)
+
+          // Run the process
+          ShadowSystemFunctions.updateDropShadowTransforms()
+          const result = getComponent(dropShadowEntity, TransformComponent).scale
+
+          // Check the result (output)
+          assertVec.anyApproxNotEq(result, Initial, 3, 0.01)
+          assertVec.approxEq(result, Expected, 3, 0.01)
+        })
+      })
+
+      describe('.. when there is an intersection and intersection.face between the scene objects and a ray casted from entity.DropShadowComponent.entity.TransformComponent.position.world ...', () => {
+        let material = undefined as Material | undefined
+        let sceneEntity = UndefinedEntity
+        let priorityEntity = UndefinedEntity
+        let dropShadowEntity = UndefinedEntity
+        const center = new Vector3()
+
+        beforeEach(() => {
+          const sceneEntityPosition = new Vector3(0, -1.5, 0)
+          sceneEntity = createEntity() // shadowIntersectionEntity : Intersect against
+          setComponent(sceneEntity, MeshComponent, new Mesh(new BoxGeometry()))
+          setComponent(sceneEntity, VisibleComponent)
+          setComponent(sceneEntity, TransformComponent, { position: sceneEntityPosition })
+
+          priorityEntity = createEntity() // shadowCasterEntity
+          setComponent(priorityEntity, VisibleComponent)
+          setComponent(priorityEntity, TransformComponent)
+          setComponent(priorityEntity, DropShadowComponent, { entity: createEntity(), center: center })
+
+          dropShadowEntity = getComponent(priorityEntity, DropShadowComponent).entity
+          setComponent(dropShadowEntity, VisibleComponent)
+          setComponent(dropShadowEntity, TransformComponent)
+          material = new Material()
+          setComponent(dropShadowEntity, ObjectComponent, new Mesh(new BoxGeometry(), material))
+        })
+
+        afterEach(() => {
+          material = undefined
+          center.set(0, 0, 0)
+          removeEntity(sceneEntity)
+          removeEntity(priorityEntity)
+          removeEntity(dropShadowEntity)
+          sceneEntity = UndefinedEntity
+          priorityEntity = UndefinedEntity
+          dropShadowEntity = UndefinedEntity
+        })
+
+        it('... should set entity.DropShadowComponent.entity.ObjectComponent.material.opacity to the expected value', () => {
+          const Expected = 0.6
+          const Initial = 42_000
+
+          // Set the input & dependencies data
+          material!.opacity = Initial
+          const rayDir = ShadowSystemData.shadowDirection.clone()
+          const rayPos = TransformComponent.getWorldPosition(priorityEntity, new Vector3()).add(center)
+          const raycaster = new Raycaster(rayPos, rayDir)
+          const sceneObjects = [getComponent(sceneEntity, MeshComponent)]
+
+          // Sanity check (input & dependencies)
+          const intersected = raycaster.intersectObjects(sceneObjects, false)[0]
+          expect(intersected).toBeTruthy()
+          expect(intersected.face).toBeTruthy()
+          const before = ((getComponent(dropShadowEntity, ObjectComponent) as Mesh).material as Material).opacity
+          expect(before).toBe(Initial)
+          expect(before).not.toBe(Expected)
+
+          // Run the process
+          ShadowSystemFunctions.updateDropShadowTransforms()
+          const result = ((getComponent(dropShadowEntity, ObjectComponent) as Mesh).material as Material).opacity
+
+          // Check the result (output)
+          expect(result).not.toBe(Initial)
+          expect(result).toBe(Expected)
+        })
+
+        it('... should set entity.DropShadowComponent.entity.TransformComponent.rotation to the expected value', () => {
+          const Expected = new Quaternion(1, 2, 3, 4).normalize()
+          const Initial = new Quaternion(5, 6, 7, 8).normalize()
+
+          // Set the input & dependencies data
+          const rayDir = ShadowSystemData.shadowDirection.clone()
+          const rayPos = TransformComponent.getWorldPosition(priorityEntity, new Vector3()).add(center)
+          const raycaster = new Raycaster(rayPos, rayDir)
+          const sceneObjects = [getComponent(sceneEntity, MeshComponent)]
+          const intersected = raycaster.intersectObjects(sceneObjects, false)[0]
+          Expected.setFromUnitVectors(intersected.face?.normal!, Vector3_Back)
+          setComponent(dropShadowEntity, TransformComponent, { rotation: Initial })
+
+          // Sanity check (input & dependencies)
+          expect(intersected).toBeTruthy()
+          expect(intersected.face).toBeTruthy()
+          const before = getComponent(dropShadowEntity, TransformComponent).rotation
+          assertVec.approxEq(before, Initial, 4)
+          assertVec.anyApproxNotEq(before, Expected, 4)
+
+          // Run the process
+          ShadowSystemFunctions.updateDropShadowTransforms()
+          const result = getComponent(dropShadowEntity, TransformComponent).rotation
+
+          // Check the result (output)
+          assertVec.anyApproxNotEq(result, Initial, 4, 0.01)
+          assertVec.approxEq(result, Expected, 4, 0.01)
+        })
+
+        it('... should set entity.DropShadowComponent.entity.TransformComponent.scale to the expected value', () => {
+          const Expected = new Vector3(0, 0, 0)
+          const Initial = new Vector3(5, 6, 7)
+
+          // Set the input & dependencies data
+          setComponent(dropShadowEntity, TransformComponent, { scale: Initial })
+          const rayDir = ShadowSystemData.shadowDirection.clone()
+          const rayPos = TransformComponent.getWorldPosition(priorityEntity, new Vector3()).add(center)
+          const raycaster = new Raycaster(rayPos, rayDir)
+          const sceneObjects = [getComponent(sceneEntity, MeshComponent)]
+          const intersected = raycaster.intersectObjects(sceneObjects, false)[0]
+
+          // Sanity check (input & dependencies)
+          expect(intersected).toBeTruthy()
+          expect(intersected.face).toBeTruthy()
+          const before = getComponent(dropShadowEntity, TransformComponent).scale
+          assertVec.approxEq(before, Initial, 4)
+          assertVec.anyApproxNotEq(before, Expected, 4)
+
+          // Run the process
+          ShadowSystemFunctions.updateDropShadowTransforms()
+          const result = getComponent(dropShadowEntity, TransformComponent).scale
+
+          // Check the result (output)
+          assertVec.anyApproxNotEq(result, Initial, 3, 0.01)
+          assertVec.approxEq(result, Expected, 3, 0.01)
+        })
+
+        it('... should set entity.DropShadowComponent.entity.TransformComponent.position to the expected value', () => {
+          const Expected = new Vector3(0, -0.99, 0)
+          const Initial = new Vector3(5, 6, 7)
+
+          // Set the input & dependencies data
+          setComponent(dropShadowEntity, TransformComponent, { position: Initial })
+          const rayDir = ShadowSystemData.shadowDirection.clone()
+          const rayPos = TransformComponent.getWorldPosition(priorityEntity, new Vector3()).add(center)
+          const raycaster = new Raycaster(rayPos, rayDir)
+          const sceneObjects = [getComponent(sceneEntity, MeshComponent)]
+          const intersected = raycaster.intersectObjects(sceneObjects, false)[0]
+
+          // Sanity check (input & dependencies)
+          expect(intersected).toBeTruthy()
+          expect(intersected.face).toBeTruthy()
+          const before = getComponent(dropShadowEntity, TransformComponent).position
+          assertVec.approxEq(before, Initial, 4)
+          assertVec.anyApproxNotEq(before, Expected, 4)
+
+          // Run the process
+          ShadowSystemFunctions.updateDropShadowTransforms()
+          const result = getComponent(dropShadowEntity, TransformComponent).position
+
+          // Check the result (output)
+          assertVec.anyApproxNotEq(result, Initial, 3, 0.01)
+          assertVec.approxEq(result, Expected, 3, 0.01)
+        })
+      })
+    })
+  }) //:: updateDropShadowTransforms
 }) //:: ShadowSystemFunctions
