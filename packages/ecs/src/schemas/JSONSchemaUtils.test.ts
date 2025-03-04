6371dd76f1f91b72503b1b40d840bba94ffdd73b
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2025-03-04 16:52:15 +0100

    tst: Unit Tests for `JSONSchemaUtils.ts` (phase 6)

--- packages/ecs/src/schemas/JSONSchemaUtils.test.ts
+++ packages/ecs/src/schemas/JSONSchemaUtils.test.ts
@@ -29,7 +29,7 @@ import { createEntity, removeEntity } from '../ComponentFunctions'
 import { createEngine, destroyEngine } from '../Engine'
 import { UndefinedEntity } from '../Entity'
 import { Kind, Schema, TArraySchema, TEnumSchema } from './JSONSchemaTypes'
-import { DeserializeSchemaValue, HasSchemaDeserializers } from './JSONSchemaUtils'
+import { DeserializeSchemaValue, HasRequiredSchema, HasSchemaDeserializers } from './JSONSchemaUtils'
 
 /**
  * @description Returns an object nested to `@param depth` levels of depth.
@@ -1136,15 +1136,77 @@ describe('HasSchemaDeserializers', () => {
 }) //:: HasSchemaDeserializers
 
 describe('HasRequiredSchema', () => {
-  it.todo('should return false if `@param schema` has no children and does not have a Kind.Required field', () => {})
-  it.todo(
-    'should return false if `@param schema` has children, does not have a Kind.Required field and none of its children have it either',
-    () => {}
-  )
-  it.todo('should return true if `@param schema` has a Kind.Required field', () => {})
+  it('should return false if `@param schema` has no children and does not have a Kind.Required field', () => {
+    const Expected = false
+    // 3. Set input & dependencies data
+    const schema = { [Kind]: 'Number', properties: undefined } as Schema
+    // 1. Sanity check (input & dependencies)
+    expect(schema.properties).toBeUndefined()
+    expect(schema[Kind]).not.toBe('Required')
+    // 2. Run the process
+    const result = HasRequiredSchema(schema)
+    // 4. Check the result (output)
+    expect(result).toBe(Expected)
+    // 5? Cleanup (dependencies)
+  })
+
+  it('should return false if `@param schema` has children, does not have a Kind.Required field and none of its children have it either', () => {
+    const Expected = false
+    // 3. Set input & dependencies data
+    const child = { [Kind]: 'Any', properties: undefined } as Schema
+    const children = {
+      child1: child,
+      child2: child,
+      child3: child
+    }
+    const schema = { [Kind]: 'Number', properties: children } as Schema
+    // 1. Sanity check (input & dependencies)
+    expect(schema.properties).not.toBeUndefined()
+    expect(schema[Kind]).not.toBe('Required')
+    // 2. Run the process
+    const result = HasRequiredSchema(schema)
+    // 4. Check the result (output)
+    expect(result).toBe(Expected)
+    // 5? Cleanup (dependencies)
+  })
+
+  it('should return true if `@param schema` has a Kind.Required field', () => {
+    const Expected = true
+    // 3. Set input & dependencies data
+    const schema = { [Kind]: 'Required', properties: undefined } as Schema
+    // 1. Sanity check (input & dependencies)
+    expect(schema.properties).toBeUndefined()
+    expect(schema[Kind]).toBe('Required')
+    // 2. Run the process
+    const result = HasRequiredSchema(schema)
+    // 4. Check the result (output)
+    expect(result).toBe(Expected)
+    // 5? Cleanup (dependencies)
+  })
+
+  /** @todo Why is this setup not returning true ?? */
   it.todo(
     'should return true if `@param schema` has children, does not have a Kind.Required field and at least one of its children have a Kind.Required field ',
-    () => {}
+    () => {
+      const Expected = true
+      // 3. Set input & dependencies data
+      const child = { [Kind]: 'Any', properties: undefined } as Schema
+      const required = { [Kind]: 'Required', properties: child } as Schema
+      const children = {
+        child1: child,
+        child2: required,
+        child3: child
+      }
+      const schema = { [Kind]: 'Number', properties: children } as Schema
+      // 1. Sanity check (input & dependencies)
+      expect(schema.properties).not.toBeUndefined()
+      expect(schema[Kind]).not.toBe('Required')
+      // 2. Run the process
+      const result = HasRequiredSchema(schema)
+      // 4. Check the result (output)
+      expect(result).toBe(Expected)
+      // 5? Cleanup (dependencies)
+    }
   )
 }) //:: HasRequiredSchema
 
