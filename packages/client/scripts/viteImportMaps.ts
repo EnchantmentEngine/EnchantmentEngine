a35ad57bcc1dd97666c7df4f20432517d230cb67
HexaField <joshfield999@gmail.com>
2025-03-22 16:58:31 +1100

    licenses

--- packages/client/scripts/viteImportMaps.ts
+++ packages/client/scripts/viteImportMaps.ts
@@ -1,3 +1,28 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Infinite Reality Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Infinite Reality Engine team.
+
+All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023 
+Infinite Reality Engine. All Rights Reserved.
+*/
+
 /**
  * https://github.com/vitejs/vite/issues/2483#issuecomment-1912618966
  */
@@ -22,7 +47,7 @@ export default (command: 'build' | 'serve', entries: { [key: string]: string }):
          * Specify the entry points for pre-optimization.
          */
         // include: Object.values(entries),
-        exclude: [...Object.keys(entries), ...Object.values(entries)],
+        exclude: [...Object.keys(entries), ...Object.values(entries)]
       },
       build: {
         rollupOptions: {
@@ -62,7 +87,7 @@ export default (command: 'build' | 'serve', entries: { [key: string]: string }):
         return code
       }
     }
-  },
+  }
   // load(id) {
   //   for (const [from, to] of Object.entries(entries)) {
   //     if (id.endsWith(from + '/xrengine.config.ts')) {
