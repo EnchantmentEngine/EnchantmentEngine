93e3220c7588edc0da30c72d0509308ae2d2ea9f
HexaField <joshfield999@gmail.com>
2025-04-10 16:05:19 +1000

    Enhance JSONSchema utility: add support for $comment option and update type definitions

--- packages/ecs/src/schemas/JSONSchemaUtils.ts
+++ packages/ecs/src/schemas/JSONSchemaUtils.ts
@@ -6,8 +6,8 @@ Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
 https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
 The License is based on the Mozilla Public License Version 1.1, but Sections 14
-and 15 have been added to cover use of software over a computer network and 
-provide for limited attribution for the Original Developer. In addition, 
+and 15 have been added to cover use of software over a computer network and
+provide for limited attribution for the Original Developer. In addition,
 Exhibit A has been modified to be consistent with Exhibit B.
 
 Software distributed under the License is distributed on an "AS IS" basis,
@@ -19,7 +19,7 @@ The Original Code is Infinite Reality Engine.
 The Original Developer is the Initial Developer. The Initial Developer of the
 Original Code is the Infinite Reality Engine team.
 
-All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023 
+All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023
 Infinite Reality Engine. All Rights Reserved.
 */
 
@@ -764,12 +764,13 @@ export const GenerateJSONSchema = <T extends Schema>(schema: T): JSONSchema | un
     jsonSchema.$id = schema.options.id
   }
 
+  // Add $comment if provided in options
+  if (schema.options?.$comment) {
+    jsonSchema.$comment = schema.options.$comment
+  }
+
   return jsonSchema
 }
-
-/**
- * @todo we may replace period separated paths with the JSON Pointer spec
- */
 
 /**
  * Temporary type for JSON Schema until we implement a proper package to handle this.
@@ -781,7 +782,13 @@ export type JSONSchema = {
   minItems?: number
   maxItems?: number
   oneOf?: JSONSchema[]
+  $comment?: string
+  $id?: string
 }
+
+/**
+ * @todo we may replace period separated paths with the JSON Pointer spec
+ */
 
 /**
  * Recursively flattens a JSONSchema so that nested properties are represented
