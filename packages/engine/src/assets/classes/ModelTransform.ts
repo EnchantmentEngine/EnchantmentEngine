c0e70ac9cf12752b06f761e90d4b6d61eecd2eeb
hzraslan <hzraslan@gmail.com>
2025-05-08 12:22:36 -0500

    optimizations

--- packages/engine/src/assets/classes/ModelTransform.ts
+++ packages/engine/src/assets/classes/ModelTransform.ts
@@ -133,6 +133,7 @@ export type ModelTransformParameters = ExtractedImageTransformParameters & {
 
   simplifyRatio: number
   simplifyErrorThreshold: number
+  adaptiveSimplification: boolean
 
   modelFormat: ModelFormat
 
@@ -195,8 +196,9 @@ export const DefaultModelTransformParameters: ModelTransformParameters = {
   mipmap: true,
   textureCompressionQuality: 128,
   maxTextureSize: 1024,
-  simplifyRatio: 1.0,
-  simplifyErrorThreshold: 0.001,
+  simplifyRatio: 1,
+  simplifyErrorThreshold: 0.01,
+  adaptiveSimplification: false,
   resources: {
     geometries: [],
     images: []
