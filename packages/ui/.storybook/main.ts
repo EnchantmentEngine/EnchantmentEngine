3e71abead64cefee253e522e7db611541f45216e
Adrian <agcacho22@gmail.com>
2025-05-09 10:51:44 -0700

    init commit

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -42,7 +42,8 @@ const config: StorybookConfig = {
     // '../src/components/editor/**/*.stories.@(js|jsx|ts|tsx)',
     '../src/components/editor/AudioVolumeVisualizer/index.stories.@(js|jsx|ts|tsx)',
     '../src/components/tailwind/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/icons/**/*.stories.@(js|jsx|ts|tsx)'
+    '../src/icons/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/components/Settings/index.stories.tsx'
   ],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
