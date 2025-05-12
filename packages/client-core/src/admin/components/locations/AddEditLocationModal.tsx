ab2d8e6f9c00aba0dbc5c8d3c8d0fda69e69b3a2
Muhammad Shafiq <shafiqkhuidad@gmail.com>
2025-05-13 00:44:41 +0500

    Add EmbedCodeField to AddEditLocationModal for scene URL generation

--- packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
+++ packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
@@ -5,8 +5,8 @@ Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
 https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
 The License is based on the Mozilla Public License Version 1.1, but Sections 14
-and 15 have been added to cover use of software over a computer network and 
-provide for limited attribution for the Original Developer. In addition, 
+and 15 have been added to cover use of software over a computer network and
+provide for limited attribution for the Original Developer. In addition,
 Exhibit A has been modified to be consistent with Exhibit B.
 Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
@@ -14,10 +14,11 @@ specific language governing rights and limitations under the License.
 The Original Code is Infinite Reality Engine.
 The Original Developer is the Initial Developer. The Initial Developer of the
 Original Code is the Infinite Reality Engine team.
-All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023 
+All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023
 Infinite Reality Engine. All Rights Reserved.
 */
 
+import { EmbedCodeField } from '@ir-engine/client-core/src/common/components/EmbedCodeField'
 import { ModalState } from '@ir-engine/client-core/src/common/services/ModalState'
 import { useFind, useMutation } from '@ir-engine/common'
 import { config } from '@ir-engine/common/src/config'
@@ -321,7 +322,7 @@ export default function AddEditLocationModal(props: AddEditLocationModalProps) {
           return lod
         })
         lods.set(defaults)
-        let fileLODs = lods.value as LODVariantDescriptor[]
+        const fileLODs = lods.value as LODVariantDescriptor[]
 
         const lodVariantParams: ModelTransformParameters[] = fileLODs.map((lod) => ({
           ...lod.params
@@ -512,7 +513,7 @@ export default function AddEditLocationModal(props: AddEditLocationModalProps) {
         </div>
 
         <div className="h-fit max-h-[60vh] w-full overflow-y-auto">
-          <div className="relative grid w-full gap-6">
+          <div className="relative grid w-full gap-4">
             {errors.serverError.value && <p className="mb-3 text-red-700">{errors.serverError.value}</p>}
             {
               <div className={location ? 'border-y border-y-ui-outline' : ''}>
@@ -611,6 +612,20 @@ export default function AddEditLocationModal(props: AddEditLocationModalProps) {
                 </div>
               </div>
             </div>
+            {scene.value && scenes.status === 'success' && (
+              <div className="border-t border-t-ui-outline py-6">
+                <EmbedCodeField
+                  url={(() => {
+                    const sceneResource = scenes.data.find((s) => s.id === scene.value)
+                    if (sceneResource) {
+                      const sceneName = sceneResource.key.split('/').pop()!.replace('.gltf', '')
+                      return `${config.client.clientUrl}/location/${sceneName}`
+                    }
+                    return ''
+                  })()}
+                />
+              </div>
+            )}
           </div>
         </div>
 
