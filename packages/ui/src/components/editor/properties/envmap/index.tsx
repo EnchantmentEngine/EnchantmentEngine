a11cd52a7f85a7fb15c7abcde5c571b470744a19
Tavius Koktavy <tavius@nyu.edu>
2025-05-20 15:50:50 -0400

    Temp Fix: Filter "Probes" from the EnvMap dropdown

--- packages/ui/src/components/editor/properties/envmap/index.tsx
+++ packages/ui/src/components/editor/properties/envmap/index.tsx
@@ -6,8 +6,8 @@ Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
 https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
 The License is based on the Mozilla Public License Version 1.1, but Sections 14
-and 15 have been added to cover use of software over a computer network and 
-provide for limited attribution for the Original Developer. In addition, 
+and 15 have been added to cover use of software over a computer network and
+provide for limited attribution for the Original Developer. In addition,
 Exhibit A has been modified to be consistent with Exhibit B.
 
 Software distributed under the License is distributed on an "AS IS" basis,
@@ -48,8 +48,12 @@ import SelectInput from '../../input/Select'
 
 /**
  * EnvMapSourceOptions array containing SourceOptions for Envmap
+ * Note: Probes option is currently broken and has been commented out
+ * For the latest discussion see: https://tsu.atlassian.net/browse/IR-7898 and https://theinfinitereality.slack.com/archives/C070ZGB7Q9E/p1746568563888759
  */
-const EnvMapSourceOptions = Object.values(EnvMapSourceType).map((value) => ({ label: value, value }))
+const EnvMapSourceOptions = Object.values(EnvMapSourceType)
+  .filter((value) => value !== EnvMapSourceType.Probes) // Filter out the Probes option
+  .map((value) => ({ label: value, value }))
 
 /**
  * EnvMapEditor provides the editor view for environment map property customization.
