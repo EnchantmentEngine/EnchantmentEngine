4a1afe6f730e7fd0cb14cc555f43f7fcf88a7aef
Tavius Koktavy <tavius@nyu.edu>
2025-05-20 11:54:20 -0400

    Use URL hash for cache busting instead of timestamp

--- packages/editor/src/functions/uploadEnvMapBake.ts
+++ packages/editor/src/functions/uploadEnvMapBake.ts
@@ -6,8 +6,8 @@ Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
 https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
 The License is based on the Mozilla Public License Version 1.1, but Sections 14
-and 15 have been added to cover use of software over a computer network and 
-provide for limited attribution for the Original Developer. In addition, 
+and 15 have been added to cover use of software over a computer network and
+provide for limited attribution for the Original Developer. In addition,
 Exhibit A has been modified to be consistent with Exhibit B.
 
 Software distributed under the License is distributed on an "AS IS" basis,
@@ -100,11 +100,9 @@ export const uploadBPCEMBakeToServer = async (entity: Entity) => {
   const editorState = getState(EditorState)
   const sceneName = editorState.sceneName!
   const projectName = editorState.projectName!
-  const timestamp = Date.now()
-  // Add a cache-busting timestamp to the filename to ensure the envmap updates each bake
   const filename = isSceneRootEntity
-    ? `${sceneName}.envmap.${timestamp}.ktx2`
-    : `${sceneName}-${nameComponent.replace(' ', '-')}.${timestamp}.ktx2`
+    ? `${sceneName}.envmap.ktx2`
+    : `${sceneName}-${nameComponent.replace(' ', '-')}.ktx2`
 
   const currentSceneDirectory = getState(EditorState).scenePath!.split('/').slice(0, -1).join('/')
   const url = (
@@ -112,8 +110,6 @@ export const uploadBPCEMBakeToServer = async (entity: Entity) => {
   )[0]
 
   const cleanURL = new URL(url)
-  cleanURL.hash = ''
-  cleanURL.search = ''
 
   setComponent(entity, EnvMapBakeComponent, { envMapOrigin: cleanURL.href })
 }
