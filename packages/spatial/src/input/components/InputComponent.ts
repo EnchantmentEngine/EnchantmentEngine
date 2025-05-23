6d16e0d6aeccf13ba9897c0f9abcda2ce2305943
Tavius Koktavy <tavius@nyu.edu>
2025-05-23 12:48:47 -0400

    Revert "Remove unused AnimateScaleComponent"
    This reverts commit fdc5b8621f270828de1ea8891e1746558c6b2b5b.

--- packages/spatial/src/input/components/InputComponent.ts
+++ packages/spatial/src/input/components/InputComponent.ts
@@ -391,6 +391,15 @@ export const InputComponent = defineComponent({
     //   // collider.collisionLayer.set(collider.collisionLayer.value | CollisionGroups.Input)
     // }, [])
 
+    /** @todo - fix */
+    // useLayoutEffect(() => {
+    //   if (!input.inputSources.length || !input.grow.value) return
+    //   setComponent(entity, AnimateScaleComponent)
+    //   return () => {
+    //     removeComponent(entity, AnimateScaleComponent)
+    //   }
+    // }, [input.inputSources, input.grow])
+
     return null
   }
 })
