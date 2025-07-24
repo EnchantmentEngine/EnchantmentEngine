5d132f040f10d1608a355f6476717f7f043fead4
MoizAdnan <moizadnan21@gmail.com>
2025-07-24 17:22:51 +0500

    moderation

--- packages/server-core/src/moderation/moderation/migrations/20250206154010_moderation.ts
+++ packages/server-core/src/moderation/moderation/migrations/20250206154010_moderation.ts
@@ -12,6 +12,10 @@ export async function up(knex: Knex): Promise<void> {
     table.string('reportDetails', 1050).notNullable()
     table.string('abuseReason', 255).notNullable()
     table.string('type', 255).notNullable()
+    table.string('reportedUserIpAddress', 255).nullable()
+    table.increments('referenceNumber').unsigned().nullable().unique()
+    table.string('reportedUserCountry', 100).nullable()
+    table.string('reportingUserCountry', 100).nullable()
     //@ts-ignore
     table.uuid('reportedUserId', 36).collate('utf8mb4_bin')
 
